d34e6dca553ba730db9f9856cac9438229cc3419
lucahrng <lucahornung@googlemail.com>
2026-07-04 12:49:24 +0200

    Create Lib scoring.ts

---
+++ Lib/Lib scoring.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export function supabaseAdmin() {
+  const { createClient: create } = require("@supabase/supabase-js");
+  return create(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+}
